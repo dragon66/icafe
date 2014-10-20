8c4a3681e6fced599047ee072080489809250e0f
dragon66 <yuwen_66@yahoo.com>
2014-10-20 02:42:57 -0400

    Test merge with mergeTiffImagesEx

--- src/cafe/test/TestTiffMerge.java
+++ src/cafe/test/TestTiffMerge.java
@@ -1,13 +1,11 @@
 package cafe.test;
 
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import cafe.image.tiff.TIFFTweaker;
-import cafe.io.FileCacheRandomAccessInputStream;
 import cafe.io.FileCacheRandomAccessOutputStream;
-import cafe.io.RandomAccessInputStream;
 import cafe.io.RandomAccessOutputStream;
 
 import java.io.FileOutputStream;
@@ -15,19 +13,11 @@ import java.io.FileOutputStream;
 public class TestTiffMerge {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		FileInputStream is1 = new FileInputStream(args[0]);
-		FileInputStream is2 = new FileInputStream(args[1]);
 		FileOutputStream out = new FileOutputStream(args[2]);
-		RandomAccessInputStream src1 = new FileCacheRandomAccessInputStream(is1);
-		RandomAccessInputStream src2 = new FileCacheRandomAccessInputStream(is2);
 		RandomAccessOutputStream dest = new FileCacheRandomAccessOutputStream(out);
-		TIFFTweaker.mergeTiffImages(src1, src2, dest);
+		TIFFTweaker.mergeTiffImagesEx(dest, new File(args[0]), new File(args[1]));
 		// Release resources
-		src1.close();
-		src2.close();
 		dest.close();
-		is1.close();
-		is2.close();
 		out.close();
 	}
 }
