d94d2939b111b059c8da214295cc554740663997
dragon66 <yuwen_66@yahoo.com>
2014-12-07 10:36:03 -0500

    New Float and Double TIFF field type wrappers

---
+++ src/cafe/image/tiff/DoubleField.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2014 by Wen Yu.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Any modifications to this file must keep this entire header intact.
+ */
+
+package cafe.image.tiff;
+
+import java.io.IOException;
+
+import cafe.io.RandomAccessOutputStream;
+
+/**
+ * TIFF FieldType.DOUBLE wrapper
+ *
+ * @author Wen Yu, yuwen_66@yahoo.com
+ * @version 1.0 12/04/2014
+ */
+public class DoubleField extends TiffField<double[]> {
+
+	public DoubleField(short tag, double[] data) {
+		super(tag, FieldType.DOUBLE, data.length);
+		this.data = data;
+	}
+
+	@Override
+	protected int writeData(RandomAccessOutputStream os, int toOffset)
+			throws IOException {
+		//
+		dataOffset = toOffset;
+		os.writeInt(toOffset);
+		os.seek(toOffset);
+		
+		for (double value : data)
+			os.writeDouble(value);
+		
+		toOffset += (data.length << 3);
+		
+		return toOffset;
+	}
+}
---
+++ src/cafe/image/tiff/FloatField.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2014 by Wen Yu.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Any modifications to this file must keep this entire header intact.
+ */
+
+package cafe.image.tiff;
+
+import java.io.IOException;
+
+import cafe.io.RandomAccessOutputStream;
+
+/**
+ * TIFF FieldType.FLOAT wrapper
+ *
+ * @author Wen Yu, yuwen_66@yahoo.com
+ * @version 1.0 12/04/2014
+ */
+public class FloatField extends TiffField<float[]> {
+
+	public FloatField(short tag, float[] data) {
+		super(tag, FieldType.FLOAT, data.length);
+		this.data = data;
+	}
+
+	protected int writeData(RandomAccessOutputStream os, int toOffset) throws IOException {
+		if (data.length == 1) {
+			dataOffset = (int)os.getStreamPointer();
+			os.writeFloat(data[0]);
+		} else {
+			dataOffset = toOffset;
+			os.writeInt(toOffset);
+			os.seek(toOffset);
+			
+			for (float value : data)
+				os.writeFloat(value);
+			
+			toOffset += (data.length << 2);
+		}
+		return toOffset;
+	}
+}
