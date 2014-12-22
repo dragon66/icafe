ca53f3934dbae89d61c5a1f6cbd5ddb5c871e5e2
dragon66 <yuwen_66@yahoo.com>
2014-12-22 02:04:21 -0500

    Renamed from ColorType

---
+++ src/cafe/image/png/ColorFormat.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2014 by Wen Yu.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Any modifications to this file must keep this entire header intact.
+ */
+
+package cafe.image.png;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Define PNG image color formats
+ * 
+ * @author Wen Yu, yuwen_66@yahoo.com 
+ * @version 1.0 07/29/2013
+ */
+public enum ColorFormat {	
+	// Image color formats
+	GRAY_SCALE(0, "Gray-scale: each pixel is a grayscale sample."),
+	TRUE_COLOR(2, "True-color: each pixel is a R,G,B triple."),
+	INDEX_COLOR(3, "Index-color: each pixel is a palette index; a PLTE chunk must appear."), 
+	GRAY_SCALE_WITH_ALPHA(4, "Gray-scale-with-alpha: each pixel is a grayscale sample, followed by an alpha sample."), 
+	TRUE_COLOR_WITH_ALPHA(6, "True-color-with-alpha: each pixel is a R,G,B triple, followed by an alpha sample."), 
+    
+	UNKNOWN(999, "UNKNOWN"); // We don't know this color format
+	
+	private ColorFormat(int value, String description)
+    {
+    	this.value = value;
+        this.description = description;	
+    }    
+    
+    public String getDescription()
+    {
+    	return this.description;
+    }
+    
+    public int getValue()
+    {
+    	return this.value;
+    }
+      
+    @Override
+    public String toString() {return "Image color format: " + getValue() + " - " + description;}
+    
+    public static ColorFormat fromInt(int value) {
+       	ColorFormat colorFormat = intMap.get(value);
+    	if (colorFormat == null)
+    	   return UNKNOWN;
+   		return colorFormat;
+    }
+    
+    private static final Map<Integer, ColorFormat> intMap = new HashMap<Integer, ColorFormat>();
+    
+    static
+    {
+      for(ColorFormat color : values()) {
+          intMap.put(color.getValue(), color);
+      }
+    }   
+    
+    private final String description;
+    private final int value;
+}
