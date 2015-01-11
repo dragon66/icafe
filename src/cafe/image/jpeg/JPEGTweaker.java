46ad6abf92d5d608e133aafe3b5654abf508a284
dragon66 <yuwen_66@yahoo.com>
2015-01-10 20:26:14 -0500

    Revised to use StringUtils and IRB

--- src/cafe/image/jpeg/JPEGTweaker.java
+++ src/cafe/image/jpeg/JPEGTweaker.java
@@ -58,7 +58,7 @@ import cafe.image.ImageType;
 import cafe.image.writer.ImageWriter;
 import cafe.image.meta.exif.Exif;
 import cafe.image.meta.icc.ICCProfile;
-import cafe.image.meta.iptc.IPTCDataSet;
+import cafe.image.meta.photoshop.IRBReader;
 import cafe.image.meta.photoshop.ImageResourceID;
 import cafe.image.tiff.IFD;
 import cafe.image.tiff.TIFFTweaker;
@@ -75,20 +75,6 @@ import cafe.util.ArrayUtils;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.CDATASection;
-import org.w3c.dom.Comment;
-import org.w3c.dom.Document;
-import org.w3c.dom.DocumentType;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.ProcessingInstruction;
-import org.w3c.dom.Text;
-import org.xml.sax.SAXException;
 
 /**
  * JPEG image tweaking tool
@@ -718,76 +704,6 @@ public class JPEGTweaker {
 		insertICCProfile(is, os, icc_profile.getData());
 	}
 	
-	private static void printNode(Node node, String indent) {
-		switch(node.getNodeType()) {
-	        case Node.DOCUMENT_NODE: {
-	            Node child = node.getFirstChild();
-	            while(child != null) {
-	            	printNode(child, indent);
-	            	child = child.getNextSibling();
-	            }
-	            break;
-	        } 
-	        case Node.DOCUMENT_TYPE_NODE: {
-	            DocumentType doctype = (DocumentType) node;
-	            System.out.println("<!DOCTYPE " + doctype.getName() + ">");
-	            break;
-	        }
-	        case Node.ELEMENT_NODE: { // Element node
-	            Element ele = (Element) node;
-	            System.out.print(indent + "<" + ele.getTagName());
-	            NamedNodeMap attrs = ele.getAttributes(); 
-	            for(int i = 0; i < attrs.getLength(); i++) {
-	                Node a = attrs.item(i);
-	                System.out.print(" " + a.getNodeName() + "='" + 
-	                          StringUtils.escapeXML(a.getNodeValue()) + "'");
-	            }
-	            System.out.println(">");
-
-	            String newindent = indent + "    ";
-	            Node child = ele.getFirstChild();
-	            while(child != null) {
-	            	printNode(child, newindent);
-	            	child = child.getNextSibling();
-	            }
-
-	            System.out.println(indent + "</" + ele.getTagName() + ">");
-	            break;
-	        }
-	        case Node.TEXT_NODE: {
-	            Text textNode = (Text)node;
-	            String text = textNode.getData().trim();
-	            if ((text != null) && text.length() > 0)
-	                System.out.println(indent + StringUtils.escapeXML(text));
-	            break;
-	        }
-	        case Node.PROCESSING_INSTRUCTION_NODE: {
-	            ProcessingInstruction pi = (ProcessingInstruction)node;
-	            System.out.println(indent + "<?" + pi.getTarget() +
-	                               " " + pi.getData() + "?>");
-	            break;
-	        }
-	        case Node.ENTITY_REFERENCE_NODE: {
-	            System.out.println(indent + "&" + node.getNodeName() + ";");
-	            break;
-	        }
-	        case Node.CDATA_SECTION_NODE: {           // Output CDATA sections
-	            CDATASection cdata = (CDATASection)node;
-	            System.out.println(indent + "<" + "![CDATA[" + cdata.getData() +
-	                        "]]" + ">");
-	            break;
-	        }
-	        case Node.COMMENT_NODE: {
-	        	Comment c = (Comment)node;
-	            System.out.println(indent + "<!--" + c.getData() + "-->");
-	            break;
-	        }
-	        default:
-	            System.err.println("Unknown node: " + node.getClass().getName());
-	            break;
-		}
-	}
-	
 	private static void readAPP0(InputStream is) throws IOException
 	{
 		byte[] jfif = {0x4A, 0x46, 0x49, 0x46, 0x00}; // JFIF
@@ -839,27 +755,7 @@ public class JPEGTweaker {
 		    TIFFTweaker.readIFDs(list, randInputStream);
 		   	randInputStream.close();
 		} else if(new String(ArrayUtils.subArray(buf, 0, xmp.length())).equals(xmp)) {
-			// Adobe XMP segment.
-			//Get the DOM Builder Factory
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			//Get the DOM Builder
-			DocumentBuilder builder = null;
-			try {
-				builder = factory.newDocumentBuilder();
-			} catch (ParserConfigurationException e) {
-				e.printStackTrace();
-			}
-			//Load and Parse the XML document
-			//document contains the complete XML as a Tree.
-			Document document = null;
-			try {
-				document = builder.parse(new ByteArrayInputStream(ArrayUtils.subArray(buf, xmp.length(), length - xmp.length() - 2)), "UTF-8");
-			} catch (SAXException e) {
-				e.printStackTrace();
-			}
-			
-			printNode(document,"");
-			
+			StringUtils.showXML(ArrayUtils.subArray(buf, xmp.length(), length - xmp.length() - 2));
 			// For comparison purpose only
 			//System.out.println(new String(ArrayUtils.subArray(buf, xmp.length(), length - xmp.length() - 2), "utf-8"));
   		}
@@ -933,213 +829,7 @@ public class JPEGTweaker {
 		
 		if(new String(data, 0, i++).equals("Photoshop 3.0")) {
 			System.out.println("Photoshop 3.0");
-			while((i+4) < data.length) {
-				String _8bim = new String(data, i, 4);
-				System.out.println("Type: " + _8bim);
-				i += 4;			
-				if(_8bim.equals("8BIM")) {
-					short id = IOUtils.readShortMM(data, i);
-					i += 2;
-					int nameLen = (data[i++]&0xff);
-					System.out.println("Name: " + new String(data, i, nameLen).trim());
-					i += nameLen;
-					if((nameLen%2) == 0) i++;
-					int size = IOUtils.readIntMM(data, i);
-					i += 4;
-					System.out.println("Size: " + size);
-					
-					ImageResourceID eId =ImageResourceID.fromShort(id); 
-					
-					if((id >= ImageResourceID.PATH_INFO0.getValue()) && (id <= ImageResourceID.PATH_INFO998.getValue())) {
-						System.out.println("PATH_INFO" + " [Value: " + StringUtils.shortToHexStringMM(id) +"]" + " - Path Information (saved paths).");
-					}
-					else if((id >= ImageResourceID.PLUGIN_RESOURCE0.getValue()) && (id <= ImageResourceID.PLUGIN_RESOURCE999.getValue())) {
-						System.out.println("PLUGIN_RESOURCE" + " [Value: " + StringUtils.shortToHexStringMM(id) +"]" + " - Plug-In resource.");
-					}
-					else if (eId == ImageResourceID.UNKNOWN) {
-						System.out.println(eId + " [Value: " + StringUtils.shortToHexStringMM(id) +"]");
-					}
-					else {
-						System.out.println(eId);
-					}
-					
-					switch (eId) {
-						case IPTC_NAA: // IPTC
-							/* Structure of an IPTC data set
-							   [Record name]    [size]   [description]
-							   ---------------------------------------
-							   (Tag marker)     1 byte   this must be 0x1c
-							   (Record number)  1 byte   always 2 for 2:xx datasets
-							   (Dataset number) 1 byte   this is what we call a "tag"
-							   (Size specifier) 2 bytes  data length (< 32768 bytes) or length of ...
-							   (Size specifier)  ...     data length (> 32767 bytes only)
-							   (Data)            ...     (its length is specified before)
-							 */					
-							int tagMarker = data[i];
-							List<IPTCDataSet> list = new ArrayList<IPTCDataSet>();
-							
-							while (tagMarker == 0x1c) {
-								i++;
-								int recordNumber = data[i++];
-								int tag = data[i++];
-								int recordSize = IOUtils.readUnsignedShortMM(data, i);
-								i += 2;
-								list.add(new IPTCDataSet(recordNumber, tag, recordSize, data, i));
-								i += recordSize;
-								
-								tagMarker = data[i];							
-							}
-							
-							if(size%2 != 0) i++;
-								
-							for(IPTCDataSet iptc : list) {
-								iptc.print();
-							}
-							
-							break;
-						case JPEG_QUALITY: // PhotoShop Save As Quality
-							// index 0: Quality level
-							int value = IOUtils.readShortMM(data, i);
-							i += 2;
-							switch (value) {
-								case 0xfffd:
-									System.out.print("Quality 1 (Low)");
-									break;
-								case 0xfffe:
-									System.out.print("Quality 2 (Low)");
-									break;
-								case 0xffff:
-									System.out.print("Quality 3 (Low)");
-									break;
-								case 0x0000:
-									System.out.print("Quality 4 (Low)");
-									break;
-								case 0x0001:
-									System.out.print("Quality 5 (Medium)");
-									break;
-								case 0x0002:
-									System.out.print("Quality 6 (Medium)");
-									break;
-								case 0x0003:
-									System.out.print("Quality 7 (Medium)");
-									break;
-								case 0x0004:
-									System.out.print("Quality 8 (High)");
-									break;
-								case 0x0005:
-									System.out.print("Quality 9 (High)");
-									break;
-								case 0x0006:
-									System.out.print("Quality 10 (Maximum)");
-									break;
-								case 0x0007:
-									System.out.print("Quality 11 (Maximum)");
-									break;
-								case 0x0008:
-									System.out.print("Quality 12 (Maximum)");
-									break;
-								default:
-							}
-							
-							int format = IOUtils.readShortMM(data, i);
-							i += 2;
-							System.out.print(" : ");
-							
-							switch (format) {
-								case 0x0000:
-									System.out.print("Standard Format");
-									break;
-								case 0x0001:
-									System.out.print("Optimised Format");
-									break;
-								case 0x0101:
-									System.out.print("Progressive Format");
-									break;
-								default:
-							}
-							
-							int progressiveScans = IOUtils.readShortMM(data, i);
-							i += 2;
-							System.out.print(" : ");
-							
-							switch (progressiveScans) {
-								case 0x0001:
-									System.out.print("3 Scans");
-									break;
-								case 0x0002:
-									System.out.print("4 Scans");
-									break;
-								case 0x0003:
-									System.out.print("5 Scans");
-									break;
-								default:
-							}
-							
-							System.out.println(" - Plus 1 byte unknown trailer value = " + data[i++]); // Always seems to be 0x01
-							if(size%2 != 0) i++;
-							break;
-						case THUMBNAIL_RESOURCE_PS4:
-						case THUMBNAIL_RESOURCE_PS5:	
-							int thumbnailFormat = IOUtils.readIntMM(data, i); //1 = kJpegRGB. Also supports kRawRGB (0).
-							i += 4;
-							switch (thumbnailFormat) {
-								case 1:
-									System.out.println("Thumbnail format: KJpegRGB");
-									break;
-								case 0:
-									System.out.println("Thumbnail format: KRawRGB");
-									break;
-							}
-							int width = IOUtils.readIntMM(data, i);
-							System.out.println("Thumbnail width: " + width);
-							i += 4;
-							int height = IOUtils.readIntMM(data, i);
-							System.out.println("Thumbnail height: " + height);
-							i += 4;
-							// Padded row bytes = (width * bits per pixel + 31) / 32 * 4.
-							int widthBytes = IOUtils.readIntMM(data, i);
-							System.out.println("Padded row bytes: " + widthBytes);
-							i += 4;
-							// Total size = widthbytes * height * planes
-							int totalSize = IOUtils.readIntMM(data, i);
-							System.out.println("Total size: "  + totalSize);
-							i += 4;
-							// Size after compression. Used for consistency check.
-							int sizeAfterCompression = IOUtils.readIntMM(data, i);
-							System.out.println("Size after compression: " + sizeAfterCompression);
-							i += 4;
-							short bitsPerPixel = IOUtils.readShortMM(data, i); // Bits per pixel. = 24
-							System.out.println("Bits per pixel: " + bitsPerPixel);
-							i += 2;
-							short numOfPlanes = IOUtils.readShortMM(data, i); // Number of planes. = 1
-							System.out.println("Number of planes: "  + numOfPlanes);
-							i += 2; 	
-							// JFIF data in RGB format. For resource ID 1033 (0x0409) the data is in BGR format.
-							i += sizeAfterCompression; 
-							if(size%2 != 0) i++;
-							break;
-						case VERSION_INFO:
-							System.out.println("Version: " + StringUtils.byteArrayToHexString(ArrayUtils.subArray(data, i, 4)));
-							i += 4;
-                            System.out.println("Has Real Merged Data: " + ((data[i++]!=0)?"True":"False"));
-                            int writer_size = IOUtils.readIntMM(data, i);
-                            i += 4;
-                            System.out.println("Writer name: " + new String(data, i, writer_size*2, "UTF-16BE"));
-                            i += writer_size*2;
-                            int reader_size = IOUtils.readIntMM(data, i);
-                            i += 4;
-                            System.out.println("Reader name: " + new String(data, i, reader_size*2, "UTF-16BE"));
-                            i += reader_size*2;
-                            System.out.println("File Version: " + StringUtils.byteArrayToHexString(ArrayUtils.subArray(data, i, 4)));                           
-                            i += 4;
-                            if(size%2 != 0) i++;
-                            break;
-						default:							
-							i += size;
-							if(size%2 != 0) i++;
-					}					
-				}
-			}
+			new IRBReader(ArrayUtils.subArray(data, i, data.length - i)).read();			
 		}		
 	}
 	
