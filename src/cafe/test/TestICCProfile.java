19e346e9fc76a6810400c2f801452c04df032175
dragon66 <yuwen_66@yahoo.com>
2015-01-13 09:10:39 -0500

    Change due to ICCProfile change

--- src/cafe/test/TestICCProfile.java
+++ src/cafe/test/TestICCProfile.java
@@ -9,9 +9,7 @@ public class TestICCProfile {
 
 	public static void main(String[] args) throws IOException {		
 		FileInputStream fin = new FileInputStream(args[0]);
-		ICCProfile icc_profile = new ICCProfile(fin);
-		icc_profile.showHeader();
-		icc_profile.showTagTable();
+		ICCProfile.showProfile(fin);
 		fin.close();
 	}
 }
