43e62111f5ef3100fbadb9d7412d30e4c2d6bdb4
dragon66 <yuwen_66@yahoo.com>
2015-01-17 17:17:34 -0500

    Renamed from ColorType

---
+++ src/cafe/image/ImageColorType.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2014 by Wen Yu.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Any modifications to this file must keep this entire header intact.
+ */
+
+package cafe.image;
+
+/** 
+ * Image color types
+ * 
+ * @author Wen Yu, yuwen_66@yahoo.com
+ * @version 1.0 12/17/2014
+ */
+public enum ImageColorType {
+	BILEVEL,
+	INDEXED,
+	GRAY_SCALE,
+	FULL_COLOR;
+}
