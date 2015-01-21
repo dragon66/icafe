6d6607a5861b5bd3404f65cee5d25545db3a5e23
dragon66 <yuwen_66@yahoo.com>
2015-01-21 09:45:43 -0500

    Added code to set IFDs for TIFF image

--- src/cafe/image/meta/exif/ExifReader.java
+++ src/cafe/image/meta/exif/ExifReader.java
@@ -4,12 +4,19 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import cafe.image.meta.MetadataReader;
+import cafe.image.tiff.FieldType;
 import cafe.image.tiff.IFD;
 import cafe.image.tiff.TIFFTweaker;
+import cafe.image.tiff.Tag;
 import cafe.image.tiff.TiffField;
 import cafe.image.tiff.TiffTag;
 import cafe.io.FileCacheRandomAccessInputStream;
@@ -17,6 +24,7 @@ import cafe.io.FileCacheRandomAccessOutputStream;
 import cafe.io.IOUtils;
 import cafe.io.RandomAccessInputStream;
 import cafe.io.RandomAccessOutputStream;
+import cafe.string.StringUtils;
 
 public class ExifReader implements MetadataReader {
 	private boolean loaded;
@@ -31,6 +39,10 @@ public class ExifReader implements MetadataReader {
 	
 	public ExifReader(InputStream is) throws IOException {
 		this(IOUtils.inputStreamToByteArray(is));
+	}
+	
+	public ExifReader(IFD imageIFD) {
+		ifds.add(0, imageIFD);
 	}
 	
 	public List<IFD> getIFDs() {
@@ -51,38 +63,142 @@ public class ExifReader implements MetadataReader {
 	
 	@Override
 	public void read() throws IOException {
-		RandomAccessInputStream exifIn = new FileCacheRandomAccessInputStream(new ByteArrayInputStream(data));
-	    TIFFTweaker.readIFDs(ifds, exifIn);
-	    // We have thumbnail IFD
-	    if(ifds.size() >= 2) {
-	    	containsThumbnail = true;
-	    	thumbnail = new ExifThumbnail();
-	    	IFD thumbnailIFD = ifds.get(1);
-	    	TiffField<?> field = thumbnailIFD.getField(TiffTag.JPEG_INTERCHANGE_FORMAT.getValue());
-	    	if(field != null) { // JPEG format, save as JPEG
-	    		int thumbnailOffset = field.getDataAsLong()[0];
-	    		field = thumbnailIFD.getField(TiffTag.JPEG_INTERCHANGE_FORMAT_LENGTH.getValue());
-	    		int thumbnailLen = field.getDataAsLong()[0];
-	    		exifIn.seek(thumbnailOffset);
-	    		byte[] data = new byte[thumbnailLen];
-	    		exifIn.readFully(data);
-	    		thumbnail.setImage(data, ExifThumbnail.DATA_TYPE_COMPRESSED_JPG, thumbnailIFD);
-	    	} else { // Uncompressed, save as TIFF
-	    		field = thumbnailIFD.getField(TiffTag.STRIP_OFFSETS.getValue());
-	    		if(field == null) 
-	    			field = thumbnailIFD.getField(TiffTag.TILE_OFFSETS.getValue());
-	    		if(field != null) {
-	    			 exifIn.seek(0);
-	    			 ByteArrayOutputStream bout = new ByteArrayOutputStream();
-	    			 RandomAccessOutputStream tiffout = new FileCacheRandomAccessOutputStream(bout);
-	    			 TIFFTweaker.retainPages(exifIn, tiffout, 1);
-	    			 tiffout.close(); // Auto flush when closed
-	    			 thumbnail.setImage(bout.toByteArray(), ExifThumbnail.DATA_TYPE_UNCOMPRESSED_TIFF, thumbnailIFD);
-	    		}
-	    	}
-	    }
-	    exifIn.close();
+		if(data != null) {
+			RandomAccessInputStream exifIn = new FileCacheRandomAccessInputStream(new ByteArrayInputStream(data));
+	    	TIFFTweaker.readIFDs(ifds, exifIn);		
+		    // We have thumbnail IFD
+		    if(ifds.size() >= 2) {
+		    	containsThumbnail = true;
+		    	thumbnail = new ExifThumbnail();
+		    	IFD thumbnailIFD = ifds.get(1);
+		    	TiffField<?> field = thumbnailIFD.getField(TiffTag.JPEG_INTERCHANGE_FORMAT.getValue());
+		    	if(field != null) { // JPEG format, save as JPEG
+		    		int thumbnailOffset = field.getDataAsLong()[0];
+		    		field = thumbnailIFD.getField(TiffTag.JPEG_INTERCHANGE_FORMAT_LENGTH.getValue());
+		    		int thumbnailLen = field.getDataAsLong()[0];
+		    		exifIn.seek(thumbnailOffset);
+		    		byte[] thumbnailData = new byte[thumbnailLen];
+		    		exifIn.readFully(thumbnailData);
+		    		thumbnail.setImage(thumbnailData, ExifThumbnail.DATA_TYPE_COMPRESSED_JPG, thumbnailIFD);
+		    	} else { // Uncompressed, save as TIFF
+		    		field = thumbnailIFD.getField(TiffTag.STRIP_OFFSETS.getValue());
+		    		if(field == null) 
+		    			field = thumbnailIFD.getField(TiffTag.TILE_OFFSETS.getValue());
+		    		if(field != null) {
+		    			 exifIn.seek(0);
+		    			 ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		    			 RandomAccessOutputStream tiffout = new FileCacheRandomAccessOutputStream(bout);
+		    			 TIFFTweaker.retainPages(exifIn, tiffout, 1);
+		    			 tiffout.close(); // Auto flush when closed
+		    			 thumbnail.setImage(bout.toByteArray(), ExifThumbnail.DATA_TYPE_UNCOMPRESSED_TIFF, thumbnailIFD);
+		    		}
+		    	}
+		    }
+		    exifIn.close();
+		}
 	    loaded = true;
+	}
+	
+	private static void showIFDs(Collection<IFD> list, String indent) {
+		int id = 0;
+		System.out.print(indent);
+		for(IFD currIFD : list) {
+			System.out.println("IFD #" + id);
+			showIFD(currIFD, TiffTag.class, indent);
+			id++;
+		}
+	}
+	
+	private static void showIFD(IFD currIFD, Class<? extends Tag> tagClass, String indent) {
+		// Use reflection to invoke fromShort(short) method
+		Method method = null;
+		try {
+			method = tagClass.getDeclaredMethod("fromShort", short.class);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		Collection<TiffField<?>> fields = currIFD.getFields();
+		int i = 0;
+		for(TiffField<?> field : fields) {
+			System.out.print(indent);
+			System.out.println("Field #" + i);
+			System.out.print(indent);
+			short tag = field.getTag();
+			Tag ftag = TiffTag.UNKNOWN;
+			try {
+				ftag = (Tag)method.invoke(null, tag);
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+			if (ftag == TiffTag.UNKNOWN) {
+				System.out.println("Tag: " + ftag + " [Value: 0x"+ Integer.toHexString(tag&0xffff) + "]" + " (Unknown)");
+			} else {
+				System.out.println("Tag: " + ftag);
+			}
+			FieldType ftype = field.getType();				
+			System.out.print(indent);
+			System.out.println("Field type: " + ftype);
+			int field_length = field.getLength();
+			System.out.print(indent);
+			System.out.println("Field length: " + field_length);
+			System.out.print(indent);			
+			switch (ftype)
+			{
+				case BYTE:
+				case UNDEFINED:
+					byte[] data = (byte[])field.getData();
+					if(ftag == ExifTag.EXIF_VERSION || ftag == ExifTag.FLASH_PIX_VERSION)
+						System.out.println("Field value: " + new String(data));
+					else
+						System.out.println("Field value: " + StringUtils.byteArrayToHexString(data, 0, 10));
+					break;
+				case ASCII:
+					System.out.println("Field value: " + (String)field.getData());
+					break;
+				case SHORT:
+					short[] sdata = (short[])field.getData();
+					System.out.println("Field value: " + StringUtils.shortArrayToString(sdata, 0, 10, true) + " " + ftag.getFieldDescription(sdata[0]&0xffff));
+					break;
+				case LONG:
+					int[] ldata = (int[])field.getData();
+					System.out.println("Field value: " + StringUtils.longArrayToString(ldata, 0, 10, true) + " " + ftag.getFieldDescription(ldata[0]&0xffff));
+					break;
+				case FLOAT:
+					float[] fdata = (float[])field.getData();
+					System.out.println("Field value: " + Arrays.toString(fdata));							
+					break;
+				case DOUBLE:
+					double[] ddata = (double[])field.getData();
+					System.out.println("Field value: " + Arrays.toString(ddata));
+					break;
+				case RATIONAL:
+					ldata = (int[])field.getData();	
+					System.out.println("Field value: " + StringUtils.rationalArrayToString(ldata, true));
+					break;
+				default:
+					break;					
+			}
+			i++;
+		}
+		Map<Tag, IFD> children = currIFD.getChildren();
+		
+		if(children.get(TiffTag.EXIF_SUB_IFD) != null) {
+			System.out.print(indent + ">>>>>>>>> ");
+			System.out.println("Exif SubIFD:");
+			showIFD(children.get(TiffTag.EXIF_SUB_IFD), ExifTag.class, indent + "--------- ");
+		}
+		
+		if(children.get(TiffTag.GPS_SUB_IFD) != null) {
+			System.out.print(indent + ">>>>>>>>> ");
+			System.out.println("GPS SubIFD:");
+			showIFD(children.get(TiffTag.GPS_SUB_IFD), GPSTag.class, indent + "--------- ");
+		}		
 	}
 
 	@Override
@@ -94,10 +210,12 @@ public class ExifReader implements MetadataReader {
 				e.printStackTrace();
 			}
 		}
-		
+		System.out.println("Exif reader output starts =>");
+		showIFDs(ifds, "");
 		if(containsThumbnail) {
 			System.out.println("Exif thumbnail format: " + (thumbnail.getDataType() == 1? "DATA_TYPE_COMPRESSED_JPG":"DATA_TYPE_COMPRESSED_TIFF"));
 			System.out.println("Exif thumbnail data length: " + thumbnail.getCompressedImage().length);
 		}
+		System.out.println("<= Exif reader output ends");
 	}
 }
