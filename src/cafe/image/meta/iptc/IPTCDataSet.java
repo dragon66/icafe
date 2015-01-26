810ce67fb59e2df62d38719cc592cdce6c8d690c
dragon66 <yuwen_66@yahoo.com>
2015-01-26 10:26:03 -0500

    Beautify code

--- src/cafe/image/meta/iptc/IPTCDataSet.java
+++ src/cafe/image/meta/iptc/IPTCDataSet.java
@@ -33,42 +33,60 @@ public class IPTCDataSet {
 		this.offset = offset;
 	}
 	
-	public int getRecordNumber() { return recordNumber; }				
-	public int getTag() {	return tag;	}		
-	public int getSize() { return size; }		
-	public int getOffset() { return offset; }
+	public int getRecordNumber() {
+		return recordNumber;
+	}
+	
+	public int getTag() {
+		return tag;
+	}		
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public int getOffset() {
+		return offset;
+	}
 	
 	public void print() {
-		System.out.println("Record number: " + recordNumber);
-		System.out.println("Dataset number (tag): " + tag + "[" + StringUtils.shortToHexStringMM((short)tag) + "]");
 		
 		switch (recordNumber) {
 			case 1: //Envelope Record
+				System.out.println("Record number " + recordNumber + ": Envelope Record");
 				System.out.println("Dataset name: " + IPTCEnvelopeTag.fromTag(tag));
 				break;
 			case 2: //Application Record
+				System.out.println("Record number " + recordNumber + ": Application Record");
 				System.out.println("Dataset name: " + IPTCApplicationTag.fromTag(tag));
 				break;
 			case 3: //NewsPhoto Record
+				System.out.println("Record number " + recordNumber + ": NewsPhoto Record");
 				System.out.println("Dataset name: " + IPTCNewsPhotoTag.fromTag(tag));
 				break;
 			case 7: //PreObjectData Record
+				System.out.println("Record number " + recordNumber + ": PreObjectData Record");
 				System.out.println("Dataset name: " + IPTCPreObjectDataTag.fromTag(tag));
 				break;
 			case 8: //ObjectData Record
+				System.out.println("Record number " + recordNumber + ": ObjectData Record");
 				System.out.println("Dataset name: " + IPTCObjectDataTag.fromTag(tag));
 				break;				
 			case 9: //PostObjectData Record
+				System.out.println("Record number " + recordNumber + ": PostObjectData Record");
 				System.out.println("Dataset name: " + IPTCPostObjectDataTag.fromTag(tag));
 				break;	
 			case 240: //FotoStation Record
+				System.out.println("Record number " + recordNumber + ": FotoStation Record");
 				System.out.println("Dataset name: " + IPTCFotoStationTag.fromTag(tag));
 				break;	
 			default:
+				System.out.println("Record number " + recordNumber + ": Unknown Record");
 				break;
 		}
 		
+		System.out.println("Dataset tag: " + tag + "[" + StringUtils.shortToHexStringMM((short)tag) + "]");
 		System.out.println("Dataset size: " + size);
-		System.out.println("Value: " + new String(data, offset, size).trim());
+		System.out.println("Dataset value: " + new String(data, offset, size).trim());
 	}	
 }
