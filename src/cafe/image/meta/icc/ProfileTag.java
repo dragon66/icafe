97d930aca75ad7f9d51add9687c3539d2c010368
dragon66 <yuwen_66@yahoo.com>
2015-01-25 19:57:52 -0500

    New ICC_Profile apple tag vcgt

--- src/cafe/image/meta/icc/ProfileTag.java
+++ src/cafe/image/meta/icc/ProfileTag.java
@@ -35,7 +35,6 @@ public enum ProfileTag {
 	B2D3(TagType.PUBLIC, 0x42324433, "BToD3Tag"), // Multi-dimensional transformation structure
 	BKPT(TagType.PUBLIC, 0x626b7074, "mediaBlackPointTag"), // nCIEXYZ of media black point
 	calt(TagType.PUBLIC, 0x63616C74, "calibrationDateTimeTag"), // Profile calibration date and time
-	targ(TagType.PUBLIC, 0x74617267, "charTargetTag"), //	Characterization target such as IT8/7.2
 	chad(TagType.PUBLIC, 0x63686164, "chromaticAdaptationTag"), // Converts an nCIEXYZ colour relative to the actual adopted white to the nCIEXYZ colour relative to the PCS adopted white. Required only if the chromaticity of the actual adopted white is different from that of the PCS adopted white.
 	chrm(TagType.PUBLIC, 0x6368726D, "chromaticityTag"), // Set of phosphor/colorant chromaticity
 	clro(TagType.PUBLIC, 0x636C726F, "colorantOrderTag"), // Identifies the laydown order of colorants
@@ -43,6 +42,8 @@ public enum ProfileTag {
 	clot(TagType.PUBLIC, 0x636C6F74, "colorantTableOutTag"), // Identifies the output colorants used in the profile, required only if the PCS Field is xCLR (e.g. 3CLR)
 	ciis(TagType.PUBLIC, 0x63696973, "colorimetricIntentImageStateTag"), // Indicates the image state of PCS colorimetry produced using the colorimetric intent transforms
 	cprt(TagType.PUBLIC, 0x63707274, "copyrightTag"), // Profile copyright information
+	desc(TagType.PUBLIC, 0x64657363, "profileDescriptionTag"), // Structure containing invariant and localizable versions of the profile name for displays
+	desm(TagType.PRIVATE, 0x6473636d, "appleMultilanguageDescriptionTag"),
 	dmnd(TagType.PUBLIC, 0x646D6E64, "deviceMfgDescTag"), // Displayable description of device manufacturer
 	dmdd(TagType.PUBLIC, 0x646D6464, "deviceModelDescTag"), // Displayable description of device model
 	D2B0(TagType.PUBLIC, 0x44324230, "DToB0Tag"), // Multi-dimensional transformation structure
@@ -50,30 +51,30 @@ public enum ProfileTag {
 	D2B2(TagType.PUBLIC, 0x44324232, "DToB2Tag"), // Multi-dimensional transformation structure
 	D2B3(TagType.PUBLIC, 0x44324233, "DToB3Tag"), // Multi-dimensional transformation structure
 	gamt(TagType.PUBLIC, 0x67616D74, "gamutTag"), // Out of gamut: 8-bit or 16-bit data
-	kTRC(TagType.PUBLIC, 0x6B545243, "grayTRCTag"), // Grey tone reproduction curve
-	gXYZ(TagType.PUBLIC, 0x6758595A, "greenMatrixColumnTag"), // The second column in the matrix used in matrix/TRC transforms (This column is combined with the linear green channel during the matrix multiplication).
 	gTRC(TagType.PUBLIC, 0x67545243, "greenTRCTag"), // Green channel tone reproduction curve
+	gXYZ(TagType.PUBLIC, 0x6758595A, "greenMatrixColumnTag"), // The second column in the matrix used in matrix/TRC transforms (This column is combined with the linear green channel during the matrix multiplication).
+	kTRC(TagType.PUBLIC, 0x6B545243, "grayTRCTag"), // Grey tone reproduction curve
 	lumi(TagType.PUBLIC, 0x6C756D69, "luminanceTag"), // Absolute luminance for emissive device
 	meas(TagType.PUBLIC, 0x6D656173, "measurementTag"), // Alternative measurement specification information
-	wtpt(TagType.PUBLIC, 0x77747074, "mediaWhitePointTag"), // nCIEXYZ of media white point
+	mmod(TagType.PRIVATE, 0x6d6d6f64, "MakeAndModel"),
 	ncl2(TagType.PUBLIC, 0x6E636C32, "namedColor2Tag"), // PCS and optional device representation for named colours
-	resp(TagType.PUBLIC, 0x72657370, "outputResponseTag"), //	Description of the desired device response
-	rigo(TagType.PUBLIC, 0x72696730, "perceptualRenderingIntentGamutTag"), // When present, the specified gamut is defined to be the reference medium gamut for the PCS side of both the A2B0 and B2A0 tags
 	pre0(TagType.PUBLIC, 0x70726530, "preview0Tag"), // Preview transformation: 8-bit or 16-bit data
 	pre1(TagType.PUBLIC, 0x70726531, "preview1Tag"), // Preview transformation: 8-bit or 16-bit data
 	pre2(TagType.PUBLIC, 0x70726532, "preview2Tag"), // Preview transformation: 8-bit or 16-bit data
-	desc(TagType.PUBLIC, 0x64657363, "profileDescriptionTag"), // Structure containing invariant and localizable versions of the profile name for displays
-	desm(TagType.PRIVATE, 0x6473636d, "appleMultilanguageDescriptionTag"),
-	mmod(TagType.PRIVATE, 0x6d6d6f64, "MakeAndModel"),
 	pseq(TagType.PUBLIC, 0x70736571, "profileSequenceDescTag"), // An array of descriptions of the profile sequence
 	psid(TagType.PUBLIC, 0x70736964, "profileSequenceIdentifierTag"),
+	resp(TagType.PUBLIC, 0x72657370, "outputResponseTag"), //	Description of the desired device response
+	rigo(TagType.PUBLIC, 0x72696730, "perceptualRenderingIntentGamutTag"), // When present, the specified gamut is defined to be the reference medium gamut for the PCS side of both the A2B0 and B2A0 tags
+	rig2(TagType.PUBLIC, 0x72696732, "saturationRenderingIntentGamutTag"), //	When present, the specified gamut is defined to be the reference medium gamut for the PCS side of both the A2B2 and B2A2 tags
 	rXYZ(TagType.PUBLIC, 0x7258595A, "redMatrixColumnTag"), // The first column in the matrix used in matrix/TRC transforms. (This column is combined with the linear red channel during the matrix multiplication).
 	rTRC(TagType.PUBLIC, 0x72545243, "redTRCTag"), //	Red channel tone reproduction curve
-	rig2(TagType.PUBLIC, 0x72696732, "saturationRenderingIntentGamutTag"), //	When present, the specified gamut is defined to be the reference medium gamut for the PCS side of both the A2B2 and B2A2 tags
+	targ(TagType.PUBLIC, 0x74617267, "charTargetTag"), //	Characterization target such as IT8/7.2
 	tech(TagType.PUBLIC, 0x74656368, "technologyTag"), //	Device technology information such as LCD, CRT, Dye Sublimation, etc.
+	vcgt(TagType.PRIVATE, 0x76636774, "VideoCardGammaType"),
 	vued(TagType.PUBLIC, 0x76756564, "viewingCondDescTag"), // Viewing condition description
 	view(TagType.PUBLIC, 0x76696577, "viewingConditionsTag"), // Viewing condition parameters
-	
+	wtpt(TagType.PUBLIC, 0x77747074, "mediaWhitePointTag"), // nCIEXYZ of media white point
+
 	UNKNOWN(TagType.UNKNOWN, 0xFFFFFFFF, "UnknownTag");
 	
 	public enum TagType { //
@@ -108,7 +109,7 @@ public enum ProfileTag {
     public static ProfileTag fromInt(int value) {
        	ProfileTag tag = typeMap.get(value);
     	if (tag == null) {
-    	 System.out.println("tag value " + Integer.toHexString(value) + " unknown");
+    	 System.out.println("tag value 0x" + Integer.toHexString(value) + " unknown");
     		return UNKNOWN;
     	}
    		return tag;
