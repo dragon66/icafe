1be50ac20fb9e474c7e82561debfc4dd30a6123d
dragon66 <yuwen_66@yahoo.com>
2015-02-18 16:59:56 -0500

    Added getDataSet()

--- src/cafe/image/meta/iptc/IPTC.java
+++ src/cafe/image/meta/iptc/IPTC.java
@@ -3,6 +3,7 @@ package cafe.image.meta.iptc;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 import cafe.image.meta.Metadata;
 import cafe.image.meta.MetadataType;
@@ -34,6 +35,10 @@ public class IPTC extends Metadata {
 	public IPTC(byte[] data) {
 		super(MetadataType.IPTC, data);
 		reader = new IPTCReader(data);
+	}
+	
+	public Map<String, List<IPTCDataSet>> getDataSet() {
+		return reader.getDataSet();
 	}
 	
 	public List<IPTCDataSet> getDataSet(String key) {
