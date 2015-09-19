4fa0cb1a3aeca69879c7e4bcf22d40bda428fff8
dragon66 <yuwen_66@yahoo.com>
2015-09-18 20:22:35 -0400

    Revised getScaledInstance() to work with up scaling

--- src/cafe/image/util/IMGUtils.java
+++ src/cafe/image/util/IMGUtils.java
@@ -977,43 +977,34 @@ public class IMGUtils {
     {
         int type = (img.getTransparency() == Transparency.OPAQUE) ?
             BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
-        BufferedImage ret = (BufferedImage)img;
-        int w, h;
-        if (higherQuality) {
-            // Use multi-step technique: start with original size, then
-            // scale down in multiple passes with drawImage()
-            // until the target size is reached
-            w = img.getWidth();
-            h = img.getHeight();
-        } else {
-            // Use one-step technique: scale directly from original
-            // size to target size with a single drawImage() call
-            w = targetWidth;
-            h = targetHeight;
-        }
+        BufferedImage ret = img;        
+        int w = img.getWidth(), h = img.getHeight();
         
+        // Use one-step technique: scale directly from original
+        // size to target size with a single drawImage() call
+        if(w < targetWidth || h < targetHeight || !higherQuality)
+            return scaleImage(ret, type, hint, targetWidth, targetHeight);
+       
+        // Use multi-step technique: start with original size, then
+        // scale down in multiple passes with drawImage()
+        // until the target size is reached    
         do {
-            if (higherQuality && w > targetWidth) {
+            if (w > targetWidth) {
                 w /= 2;
                 if (w < targetWidth) {
                     w = targetWidth;
                 }
             }
 
-            if (higherQuality && h > targetHeight) {
+            if (h > targetHeight) {
                 h /= 2;
                 if (h < targetHeight) {
                     h = targetHeight;
                 }
             }
 
-            BufferedImage tmp = new BufferedImage(w, h, type);
-            Graphics2D g2 = tmp.createGraphics();
-            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
-            g2.drawImage(ret, 0, 0, w, h, null);
-            g2.dispose();
-
-            ret = tmp;
+            ret = scaleImage(ret, type, hint, w, h);
+            
         } while (w != targetWidth || h != targetHeight);
 
         return ret;
@@ -1722,6 +1713,17 @@ public class IMGUtils {
 				K[i][j] = 128.0f - (cmyk[index++]&0xff);
 			}
 		}
+	}
+	
+	// From https://today.java.net/article/2007/03/30/perils-imagegetscaledinstance
+	private static BufferedImage scaleImage(BufferedImage orig, int type, Object hint, int w, int h) {
+		BufferedImage tmp = new BufferedImage(w, h, type);
+		Graphics2D g2 = tmp.createGraphics();
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+		g2.drawImage(orig, 0, 0, w, h, null);
+		g2.dispose();
+		
+		return tmp;
 	}
 	
 	// Prevent from instantiation
