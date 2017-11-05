a087c4b7437a349888e02a2e656fe9733b36f027
dragon66 <yuwen_66@yahoo.com>
2017-11-04 20:46:12 -0400

    Update StringUtils.java

--- src/com/icafe4j/string/StringUtils.java
+++ src/com/icafe4j/string/StringUtils.java
@@ -248,7 +248,7 @@ public class StringUtils {
 	    //e.g. intPart = 41
 	    coord = mod * 60;
 	    seconds = (int)coord;
-	    //Standard output of D°M'S"
+	    //Standard output of DÂ°M'S"
 	    output = degrees + '\u00B0' + minutes + "'" + seconds + "\"";
 
 	    return output;
@@ -257,7 +257,7 @@ public class StringUtils {
 	/**
 	 * Converts DMS to decimal 
 	 *
-	 * Input: latitude or longitude in the DMS format ( example: N 43° 36' 15.894")
+	 * Input: latitude or longitude in the DMS format ( example: N 43Â° 36' 15.894")
 	 * @param hemisphereOUmeridien => {W,E,S,N}
 	 * @return latitude or longitude in decimal format
 	 */
@@ -943,6 +943,45 @@ public class StringUtils {
 				throw new UnsupportedOperationException();
 			}
 		};
+	}
+	
+	/**
+	 * Convert plain text to HTML - from Stackoverflow.com
+	 * 
+	 * @param s the original text string
+	 * @return a string with the specified characters replaced by HTML entities and HTML tags
+	 */
+	public static String text2HTML(String s) {
+	    StringBuilder builder = new StringBuilder();
+	    boolean previousWasASpace = false;
+	    for( char c : s.toCharArray() ) {
+	        if( c == ' ' ) {
+	            if( previousWasASpace ) {
+	                builder.append("&nbsp;");
+	                previousWasASpace = false;
+	                continue;
+	            }
+	            previousWasASpace = true;
+	        } else {
+	            previousWasASpace = false;
+	        }
+	        switch(c) {
+	            case '<': builder.append("&lt;"); break;
+	            case '>': builder.append("&gt;"); break;
+	            case '&': builder.append("&amp;"); break;
+	            case '"': builder.append("&quot;"); break;
+	            case '\n': builder.append("<br>"); break;
+	            // We need Tab support here, because we print StackTraces as HTML
+	            case '\t': builder.append("&nbsp; &nbsp; &nbsp;"); break;  
+	            default:
+	                if( c < 128 ) {
+	                    builder.append(c);
+	                } else {
+	                    builder.append("&#").append((int)c).append(";");
+	                }    
+	        }
+	    }
+	    return builder.toString();
 	}
 	
 	public static String toHexString(byte[] bytes) {
