e046c0b944de790176c560090487b7dd15651f2b
Wen Yu <318163045@KKK6943K.maple.fg.rbc.com>
2017-11-16 14:13:30 -0500

    Copyright update

--- src/com/icafe4j/image/meta/tiff/TiffXMP.java
+++ src/com/icafe4j/image/meta/tiff/TiffXMP.java
@@ -1,3 +1,14 @@
+/**
+ * COPYRIGHT (C) 2014-2017 WEN YU (YUWEN_66@YAHOO.COM) ALL RIGHTS RESERVED.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Any modifications to this file must keep this entire header intact.
+ */
+
 package com.icafe4j.image.meta.tiff;
 
 import java.io.IOException;
--- src/com/icafe4j/util/ArrayUtils.java
+++ src/com/icafe4j/util/ArrayUtils.java
@@ -1,12 +1,13 @@
 /**
- * Copyright (c) 2014-2016 by Wen Yu.
- * All rights reserved. This program and the accompanying materials
+ * COPYRIGHT (C) 2014-2017 WEN YU (YUWEN_66@YAHOO.COM) ALL RIGHTS RESERVED.
+ *
+ * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Any modifications to this file must keep this entire header intact.
- * 
+ *
  * Change History - most recent changes go on top of previous changes
  *
  * ArrayUtils.java
