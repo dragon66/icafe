6b3ea44b3a2c78477e863d807bb87164b2628f6d
dragon66 <yuwen_66@yahoo.com>
2023-03-18 09:16:48 -0400

    Ignore bin

--- src/com/icafe4j/image/tiff/MakerNoteField.java
+++ src/com/icafe4j/image/tiff/MakerNoteField.java
@@ -10,10 +10,14 @@ import com.icafe4j.io.FileCacheRandomAccessInputStream;
 import com.icafe4j.io.IOUtils;
 import com.icafe4j.io.RandomAccessInputStream;
 import com.icafe4j.io.RandomAccessOutputStream;
+import com.icafe4j.io.WriteStrategyII;
+import com.icafe4j.io.WriteStrategyMM;
 import com.icafe4j.string.StringUtils;
 
 public class MakerNoteField extends TiffField<byte[]> {
 	//
+	private int startOffSet; // offset relative to the EXIF
+	private byte[] header = new byte[]{}; // Some maker notes start with a header
 	private short preferredEndian = IOUtils.BIG_ENDIAN;
 	private boolean isDataRead;
 	private IFD ifd;
@@ -35,11 +39,7 @@ public class MakerNoteField extends TiffField<byte[]> {
 		return StringUtils.byteArrayToHexString(data, 0, TiffField.MAX_STRING_REPR_LEN);
 	}
 	
-	public short getPreferredEndian() {
-		return preferredEndian;
-	}
-	
-	public void ensureDataRead() {
+	private void ensureDataRead() {
 		if(!isDataRead) {
 			try {
 				read();
@@ -49,9 +49,11 @@ public class MakerNoteField extends TiffField<byte[]> {
 		}
 	}
 	
-	public void read() throws IOException {
+	private void read() throws IOException {
 		if(!isDataRead) {
+			// TODO Read header to figure out preferred endian
 			RandomAccessInputStream is = new FileCacheRandomAccessInputStream(new ByteArrayInputStream(data));
+			is.setReadStrategy(null);
 			List<IFD> ifds = new ArrayList<IFD>(1);
 			TIFFTweaker.readIFD(is, ifds, ExifTag.class);
 			
@@ -65,31 +67,28 @@ public class MakerNoteField extends TiffField<byte[]> {
 		}
 	}
 	
-	public boolean isDataRead() {
-		return isDataRead;
-	}
-	
 	public void setPreferredEndian(short preferredEndian) {
 		if(preferredEndian != IOUtils.BIG_ENDIAN && preferredEndian != IOUtils.LITTLE_ENDIAN)
-			throw new IllegalArgumentException("Invalid Exif endian!");
+			throw new IllegalArgumentException("Invalid IO stream endian!");
 		this.preferredEndian = preferredEndian;
 	}	
 
 	protected int writeData(RandomAccessOutputStream os, int toOffset) throws IOException {
-		
-		if (data.length <= 4) {
-			dataOffset = (int)os.getStreamPointer();
-			byte[] tmp = new byte[4];
-			System.arraycopy(data, 0, tmp, 0, data.length);
-			os.write(tmp);
-		} else {
-			dataOffset = toOffset;
-			os.writeInt(toOffset);
-			os.seek(toOffset);
-			os.write(data);
-			toOffset += data.length;
-		}
-		
+		ensureDataRead();
+		//Remember old endian
+		short oldEndian = os.getEndian();
+		// Set preferred endian
+		if(preferredEndian != oldEndian)
+			os.setWriteStrategy(preferredEndian == IOUtils.BIG_ENDIAN? WriteStrategyMM.getInstance() : WriteStrategyII.getInstance());
+		// Write header first if any
+		if(header.length > 0)
+			os.write(header, 0, header.length);
+		// Write the ifd
+		if (ifd != null) 
+			toOffset = ifd.write(os, toOffset + header.length);
+		// Set old endian back
+		os.setWriteStrategy(oldEndian == IOUtils.BIG_ENDIAN? WriteStrategyMM.getInstance() : WriteStrategyII.getInstance());
+
 		return toOffset;
 	}
 }
