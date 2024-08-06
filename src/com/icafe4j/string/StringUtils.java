30b6b97c65309ca4b3c1f2372fc5a595e500889d
dragon66 <yuwen_66@yahoo.com>
2024-08-06 03:27:43 -0400

    Added hexStringToByteArray

--- src/com/icafe4j/string/StringUtils.java
+++ src/com/icafe4j/string/StringUtils.java
@@ -1033,6 +1033,17 @@ public class StringUtils {
 		
 		 return retVal;		 
 	}
+
+	/* s must be an even-length string. */
+        public static byte[] hexStringToByteArray(String s) {
+            int len = s.length();
+            byte[] data = new byte[len / 2];
+            for (int i = 0; i < len; i += 2) {
+                  data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                             + Character.digit(s.charAt(i+1), 16));
+            }
+            return data;
+        }
 	
 	private StringUtils(){} // Prevents instantiation	
 }
